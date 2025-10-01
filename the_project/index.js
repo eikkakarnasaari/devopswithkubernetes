e2b3f69a1ec95ec09424f88d59389543ebf785f7
eikkakarnasaari <63290403+eikkakarnasaari@users.noreply.github.com>
2025-10-01 18:56:52 +0300

    complete 1.2

---
+++ the_project/index.js
@@ -0,0 +1,15 @@
+import { Application, Router } from "@oak/oak";
+
+const router = new Router();
+const port = Number(Deno.env.get("PORT") || 3000);
+
+router.get("/", (ctx) => {
+  ctx.response.body = "Hello world";
+});
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log(`Server started in port ${port}`)
+app.listen({port});
