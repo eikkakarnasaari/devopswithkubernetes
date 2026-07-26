5ad2fc2944adf16f704224a0a2b0add636057abe
eikkakarnasaari <63290403+eikkakarnasaari@users.noreply.github.com>
2026-07-26 11:44:30 +0300

    exercise 1.7

--- log_output/main.js
+++ log_output/main.js
@@ -1,8 +1,20 @@
-const outputHash = () => {
-  const uuid = crypto.randomUUID();
+import { Hono } from "@hono/hono";
+
+const uuid = crypto.randomUUID();
+const port = Number(Deno.env.get("PORT") ?? "3000");
+const app = new Hono()
+
+app.get("/log", (c) => {
+  const timestamp = new Date().toISOString();
+  return c.text(`${timestamp}: ${uuid}`);
+});
+
+const outputString = () => {
   const timestamp = new Date().toISOString();
   console.log(`${timestamp}: ${uuid}`);
-  setTimeout(outputHash, 5000);
+  setTimeout(outputString, 5000);
 };
 
-setTimeout(outputHash, 5000);
+setTimeout(outputString, 5000);
+
+Deno.serve({ port }, app.fetch);
